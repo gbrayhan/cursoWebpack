12e611aded2c98750e38788057093bacf01712c9
Alejandro Gabriel <gbrayhan@gmail.com>
2018-08-27 21:31:55 -0400

    agregamos la nueva carpeta para el Plugin

---
+++ plugin-extract-text/index.js
@@ -0,0 +1,4 @@
+import './estilos.css';
+
+document.write('Hola Mundo desde Webpack esto es una maravilla!');
+console.log('Mensaje desde Webpack arhivo config');
---
+++ plugin-extract-text/webpack.config.js
@@ -0,0 +1,26 @@
+let path = require('path');
+
+
+module.exports = {
+  mode:'development',  
+  entry: path.resolve(__dirname,'index.js'),
+  output:{
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      //Aqui van los loaders
+      {   
+        //test: Que tipo de archivo quiero reconocer.
+        //use: que loader se va a encargar del archivo
+        test: /\.css$/,
+        use: [
+			{loader: 'style-loader'},
+			{loader: 'css-loader'}
+		]
+      }
+    ]
+  }
+}
+
