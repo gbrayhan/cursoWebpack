8d0ff969a11f7f01ba88109646c71b4398fabd30
Alejandro Gabriel <gbrayhan@gmail.com>
2018-09-01 13:47:29 -0400

    React js

---
+++ react/src/js/teachers.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Teacher} from './teacher.js'
+
+class Teachers extends Component {
+	render() {
+		return (
+			<ul className = 'teachers'>
+				{
+					this.props.data.teachers.map((teacherData) => {
+						return <Teacher {...teacherData}/ > 
+
+					})
+				}
+			</ul>
+		)
+	}
+}
+
+export {Teachers};
